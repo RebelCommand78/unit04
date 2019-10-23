75bd45d38f98190c5d761cccb3d8aa055c155c6c
Martin Stovall <martinstovall@yahoo.com>
2019-10-22 23:30:25 -0700

    adding new files

---
+++ assets/JS/app.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+      title: "What year was the Apple iphone released?:",
+      choices: ["2007", "2006", "2008", "2009"],
+      answer: "2007"
+    },
+    {
+      title: "How many iterations of iphones have there been?",
+      choices: ["14", "15", "11", "12"],
+      answer: "14"
+    },
+    {
+        title: "One of these is not a major competitor to Apple, which?",
+        choices: ["Samsung", "Microsoft", "Google", "Nike"],
+        answer: "Nike"
+      },
+      {
+        title: "Which major feature was updated to the newest iphone?",
+        choices: ["Camera","The size", "Operating System", "The Charger"],
+        answer: "Camera"
+      },
+      {
+        title: "Which of these name is a co founders of Apple inc",
+        choices: ["Steve Wozniak", "Jeff Bezos", "Paul Allen", "square brackets"],
+        answer: "Steve Wozniak"
+      },
+    
+  ];
