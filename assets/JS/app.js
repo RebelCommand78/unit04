622a86f44c5851073c47ef479d887b4ed040e368
stovallmi <martinstovall@yahoo.com>
2020-01-06 05:45:34 -0700

    near completion

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,21 +1,24 @@
 var questionsEl = document.getElementById("questions");
-var timerEl = document.getElementById("time");
+var clock = document.getElementById("time");
 var answersEl = document.getElementById("answers");
 var submitBtn = document.getElementById("submit");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
 var startBtn = document.getElementById("getStarted");
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var clockId;
 
 function getStarted() {
   var startEl = document.getElementById("startScreen");
   startEl.setAttribute("class", "hide");
 
   questionsEl.removeAttribute("class");
-  
-  timerId = setInterval(clockTick, 1000);
 
-  timerEl.textContent = time;
-  
+  clockId = setInterval(clockRun, 1000);
+
+  clock.textContent = time;
+
   getQuestion();
 
 }
@@ -30,66 +33,48 @@ var resultsContainer = document.getElementsByClassName('results');
 
 
 function getQuestion() {
-  // get current question object from array
   var currentQuestion = questions[currentQuestionIndex];
+  var titleEl = document.getElementById("questions-title");
 
-  // update title with current question
-  var titleEl = document.getElementById("question-title");
   titleEl.textContent = currentQuestion.title;
-
-  // clear out any old question answers
   answersEl.innerHTML = "";
-  
-  // loop over answers
-  currentQuestion.answers.forEach(function(answer, i) {
-    // create new button for each answer
+  currentQuestion.answers.forEach(function (response, i) {
+
     var answerNode = document.createElement("button");
-    answerNode.setAttribute("class", "answer");
-    answerNode.setAttribute("value", answer);
-
-    answerNode.textContent = i + 1 + ". " + answer;
-    
-    // attach click event listener to each answer
+    answerNode.setAttribute("class", "response");
+    answerNode.setAttribute("value", response);
+    answerNode.textContent = i + 1 + ". " + response;
     answerNode.onclick = questionClick;
-
-    // display on the page
     answersEl.appendChild(answerNode);
   });
 }
 
 function questionClick() {
-  // check if user guessed wrong
-  if (this.value !== questions[currentQuestionIndex].answer) {
-    // penalize time
+  if (this.value !== questions[currentQuestionIndex].response) {
     time -= 15;
-    
+
     if (time < 0) {
       time = 0;
     }
 
-    // display new time on page
-    timerEl.textContent = time;
 
-    // play "wrong" sound effect
+    clock.textContent = time;
+
     sfxWrong.play();
 
     feedbackEl.textContent = "Wrong!";
   } else {
-    // play "right" sound effect
-    sfxRight.play();
-
     feedbackEl.textContent = "Correct!";
   }
-  
-  // flash right/wrong feedback on page for half a second
+
   feedbackEl.setAttribute("class", "feedback");
-  setTimeout(function() {
+  setTimeout(function () {
     feedbackEl.setAttribute("class", "feedback hide");
   }, 1000);
-  
+
   // move to next question
   currentQuestionIndex++;
-  
+
   // check if we've run out of questions
   if (currentQuestionIndex === questions.length) {
     quizEnd();
@@ -99,41 +84,35 @@ function questionClick() {
 }
 
 function quizEnd() {
-  // stop timer
-  clearInterval(timerId);
-  
-  // show end screen
+  // this stops the clock
+  clearInterval(clockId);
+
   var endScreenEl = document.getElementById("end-screen");
   endScreenEl.removeAttribute("class");
-  
-  // show final score
+
   var finalScoreEl = document.getElementById("final-score");
   finalScoreEl.textContent = time;
-  
-  // hide questions section
+
   questionsEl.setAttribute("class", "hide");
 }
 
-function clockTick() {
-  // update time
+function clockRun() {
   time--;
-  timerEl.textContent = time;
+  clock.textContent = time;
 
-  // check if user ran out of time
   if (time <= 0) {
     quizEnd();
   }
 }
 
-function saveHighscore() {
-  // get value of input box
+function saveScore() {
   var initials = initialsEl.value.trim();
-  
+
   // make sure value wasn't empty
   if (initials !== "") {
     // get saved scores from localstorage, or if not any, set to empty array
     var highscores =
-    JSON.parse(window.localStorage.getItem("highscores")) || [];
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
 
     // format new score object for current user
     var newScore = {
@@ -146,44 +125,14 @@ function saveHighscore() {
     window.localStorage.setItem("highscores", JSON.stringify(highscores));
 
     // redirect to next page
-    window.location.href = "highscores.html";
+    window.location.href = "scores.html";
   }
 }
 
 function checkForEnter(event) {
-  // "13" represents the enter key
   if (event.key === "Enter") {
-    saveHighscore();
+    saveScore();
   }
 }
 
-// user clicks button to submit initials
-submitBtn.onclick = saveHighscore;
-
-var questions = [
-  {
-    title: "What year was the Apple iphone released?:",
-    answers: ["2007", "2006", "2008", "2009"],
-    answer: "2007"
-  },
-  {
-    title: "How many iterations of iphones have there been?",
-    answers: ["14", "15", "11", "12"],
-    answer: "14"
-  },
-  {
-    title: "One of these is not a major competitor to Apple, which?",
-    answers: ["Samsung", "Microsoft", "Google", "Nike"],
-    answer: "Nike"
-  },
-  {
-    title: "Which major feature was updated to the newest iphone?",
-    answers: ["Camera", "The size", "Operating System", "The Charger"],
-    answer: "Camera"
-  },
-  {
-    title: "Which of these name is a co founders of Apple inc",
-    answers: ["Steve Wozniak", "Jeff Bezos", "Paul Allen", "square brackets"],
-    answer: "Steve Wozniak"
-  }
-];
+submitBtn.onclick = saveScore;
---
+++ assets/JS/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+      title: "What year was the Apple iphone released?:",
+      answers: ["2007", "2006", "2008", "2009"],
+      response: "2007"
+    },
+    {
+      title: "How many iterations of iphones have there been?",
+      answers: ["14", "15", "11", "12"],
+      response: "14"
+    },
+    {
+      title: "One of these is not a major competitor to Apple, which?",
+      answers: ["Samsung", "Microsoft", "Google", "Nike"],
+      response: "Nike"
+    },
+    {
+      title: "Which major feature was updated to the newest iphone?",
+      answers: ["Camera", "The size", "Operating System", "The Charger"],
+      response: "Camera"
+    },
+    {
+      title: "Which of these name is a co founders of Apple inc",
+      answers: ["Steve Wozniak", "Jeff Bezos", "Paul Allen", "square brackets"],
+      response: "Steve Wozniak"
+    }
+  ];
